089157b4d7a401957d5ed810e275851345544d1c
Jason Castro <jxcast49@harrisonburg.k12.va.us>
2019-09-23 14:30:18 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 var score = 1; // Create a variable to store the score in
 
-var title = new Title('Welcome to the Game'); // Create a title element
+var title = new Title('World Builder'); // Create a title element
 var button = new Button('hello', btnPress);   // Create a button element
 var hidden = new Button('Boo!', btnPress);
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
+var button = new Button('hello', btnPress);   // Create a button element
 
-createButton('J', btnPress); // Make a button
 changeTitle('Boring Game');
 
 setInterval(btnPress, 800);
